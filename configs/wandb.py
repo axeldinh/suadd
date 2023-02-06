58511e073a3b301a95f97a74a0d23d53ba2b361e
axeldinh <axeldvc@gmail.com>
2023-02-06 14:15:39 +0100

    DATA_NAME in wandb

--- configs/wandb.py
+++ configs/wandb.py
@@ -1,3 +1,5 @@
+from configs.globals import *
+
 WANDB_PROJECT = 'AICROWD-SUADD'
 ENTITY = None # set this to team name if working in a team
 CLASSES = {i:c for i,c in enumerate([
@@ -8,3 +10,9 @@ CLASSES = {i:c for i,c in enumerate([
 CLASSES.update({255: 'unknown'})
 RAW_DATA = 'suadd'
 RAW_DATA_TEST = 'suadd-test'
+
+global DATA_NAME
+if debug:
+    DATA_NAME = RAW_DATA_TEST
+else:
+    DATA_NAME = RAW_DATA
