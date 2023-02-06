69eb1a72b3d61f3ded51315297f1d06182ebdb6d
axeldinh <axeldvc@gmail.com>
2023-02-06 13:52:01 +0100

    no background

--- configs/wandb.py
+++ configs/wandb.py
@@ -1,9 +1,10 @@
 WANDB_PROJECT = 'AICROWD-SUADD'
 ENTITY = None # set this to team name if working in a team
 CLASSES = {i:c for i,c in enumerate([
-    'background', 'water', 'asphalt', 'grass', 'human', 
+    'water', 'asphalt', 'grass', 'human', 
     'animal', 'high_vegetation', 'ground_vehicle', 'façade',
      'wire', 'garden_furniture', 'concrete', 'roof',
       'gravel', 'soil', 'primeair_pattern', 'snow'])}
 CLASSES.update({255: 'unknown'})
 RAW_DATA = 'suadd'
+RAW_DATA_TEST = 'suadd-test'
