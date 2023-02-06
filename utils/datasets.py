826d8c63030a36d99352e487044c65c27a09b0c4
axeldinh <axeldvc@gmail.com>
2023-02-06 17:19:31 +0100

    split dataset

--- utils/datasets.py
+++ utils/datasets.py
@@ -3,10 +3,12 @@ import os
 import numpy as np
 import torch
 from torchvision.io import read_image
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, Subset
 from skimage.io import imread
 
+import wandb
 
+import configs.wandb as wandb_config
 
 class ImageDataset(Dataset):
 
@@ -39,3 +41,90 @@ class ImageDataset(Dataset):
             image = self.transform(image)
 
         return image, semantic_annotation, depth_annotation
+
+def fetch_data_from_wandb():
+    run = wandb.init(
+        project=wandb_config.WANDB_PROJECT, 
+        entity=wandb_config.ENTITY,
+        name="download_data",
+        job_type="download-data"
+        )
+    artifact = run.use_artifact(wandb_config.DATA_NAME, type="dataset-suadd")
+    artifact_dir = artifact.download()
+    return artifact_dir
+
+def split_dataset(dataset: Dataset, train_ratio: float, val_ratio: float) -> tuple([Subset, Subset, Subset]):
+
+    assert train_ratio + val_ratio <= 1, "Train ratio and val ratio must be less than or equal to 1"
+
+    num_samples = len(dataset)
+    num_train = int(num_samples * train_ratio)
+    num_val = int(num_samples * val_ratio)
+    test_ratio = 1 - train_ratio - val_ratio
+    num_test = num_samples - num_train - num_val
+
+    # If want each class to be equally represented in each split, given the classes
+    train_idx = []
+    val_idx = []
+    test_idx = []
+
+    # Keep track of the number of images representing each class in each split
+    train_representation = np.zeros(len(wandb_config.CLASSES))
+    val_representation = np.zeros(len(wandb_config.CLASSES))
+    test_representation = np.zeros(len(wandb_config.CLASSES))
+
+    for i in range(len(dataset)):
+        _, semantic_annotation, _ = dataset[i]
+
+        class_in_image = torch.unique(semantic_annotation)
+        # Make it an interger list, and replace the 255 class with -1
+        class_in_image = [int(x.item()) for x in class_in_image]
+        class_in_image = [x if x != 255 else -1 for x in class_in_image]
+
+        # For each class add a vote if the class is underpresented in the split
+        train_votes = 0
+        val_votes = 0
+        test_votes = 0
+
+        for class_ in class_in_image:
+            # Ratios to take into account the final number of images in each split
+            train_value = train_representation[class_] / train_ratio,
+            val_value = val_representation[class_] / val_ratio
+            test_value =  test_representation[class_] / test_ratio
+            # Add a vote to the split with the least representation of the class, if all equal add to train
+            if train_value <= val_value and train_value <= test_value:
+                train_votes += 1
+            elif val_value < train_value and val_value <= test_value:
+                val_votes += 1
+            elif test_value < train_value and test_value < val_value:
+                test_votes += 1
+
+        # Add the image to the split with the most votes
+        if train_votes >= val_votes and train_votes >= test_votes:
+            train_idx.append(i)
+            for class_ in class_in_image:
+                train_representation[class_] += 1
+        elif val_votes > train_votes and val_votes >= test_votes:
+            val_idx.append(i)
+            for class_ in class_in_image:
+                val_representation[class_] += 1
+        elif test_votes > train_votes and test_votes > val_votes:
+            test_idx.append(i)
+            for class_ in class_in_image:
+                test_representation[class_] += 1
+
+    train_dataset = torch.utils.data.Subset(dataset, train_idx)
+    val_dataset = torch.utils.data.Subset(dataset, val_idx)
+    test_dataset = torch.utils.data.Subset(dataset, test_idx)
+
+    print("Train representation: ", train_representation)
+    print("Val representation: ", val_representation)
+    print("Test representation: ", test_representation)
+    print("Length of train: ", len(train_idx))
+    print("Length of val: ", len(val_idx))
+    print("Length of test: ", len(test_idx))
+
+    return train_dataset, val_dataset, test_dataset
+        
+
+
