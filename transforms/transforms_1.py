1a2857c7d06b492a55d3f7a3b639fbb3b36d0839
axeldinh <axeldvc@gmail.com>
2023-02-07 18:35:09 +0100

    first transforms

--- transforms/transforms_1.py
+++ transforms/transforms_1.py
@@ -15,6 +15,7 @@ class TransformSet1:
         - Random Gamma
         - Random Affine
         - Random Perspective
+        - To RGB (3 channels)
     """
 
     def __init__(self, p=0.5):
@@ -27,16 +28,29 @@ class TransformSet1:
             RandomGamma(0.3),
             T.RandomAffine(degrees=90, translate=(0.1, 0.1), scale=(0.9, 1.1), shear=10),
             T.RandomPerspective(distortion_scale=0.2, p=0.5),
-
         ])
 
-    def __call__(self, image):
+        self.transforms_mask = T.Compose([
+            T.RandomHorizontalFlip(p=p),
+            T.RandomVerticalFlip(p=p),
+            T.RandomAffine(degrees=90, translate=(0.1, 0.1), scale=(0.9, 1.1), shear=10),
+            T.RandomPerspective(distortion_scale=0.2, p=0.5),
+        ])
+
+    def __call__(self, image, is_mask=False):
         """
         Applies the transforms to a batch of torch gray images.
         :param image: torch gray images (1, height, width)
+        :param is_mask: if True, only applies horizontal, vertical flips, affine and perspective transforms
         :return: transformed torch gray images (1, height, width)
         """
 
-        image = image / 255.
+        if not is_mask and image.max() > 1.0:
+            image = image / 255.
 
-        return self.transforms(image)
+        if is_mask:
+            new_image = self.transforms_mask(image)
+        else:
+            new_image = self.transforms(image)
+
+        return new_image
