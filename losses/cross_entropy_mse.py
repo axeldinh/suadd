94e60e5cc9c895d84c692c793a15c8244f7442c7
axeldinh <axeldvc@gmail.com>
2023-02-10 18:04:29 +0100

    loss crossentropy + mse

---
+++ losses/cross_entropy_mse.py
@@ -0,0 +1,18 @@
+from torch import nn
+
+
+class CrossEntropyMSE(nn.Module):
+    def __init__(self, coeff_depth=1.0):
+        super().__init__()
+        self.semantic_loss = nn.CrossEntropyLoss()
+        self.depth_loss = nn.MSELoss()
+        self.coeff_depth = coeff_depth
+
+    def forward(self, semantic_out, depth_out, semantic, depth):
+        if semantic.dim() == 4:
+            semantic = semantic.squeeze(1)
+        semantic_loss = self.semantic_loss(semantic_out, semantic.long())
+        depth_loss = 0
+        if depth is not None:
+            depth_loss = self.depth_loss(depth_out, depth)
+        return semantic_loss + depth_loss * self.coeff_depth
