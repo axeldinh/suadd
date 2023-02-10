acdfb402d71a89fac03273ccfe2b33ebd5917e3e
axeldinh <axeldvc@gmail.com>
2023-02-10 21:06:34 +0100

    paths in globals

--- load_data.py
+++ load_data.py
@@ -1,9 +1,7 @@
 import wandb
 
-from configs.globals import *
-from configs.paths import *
 import configs.wandb as wandb_config
-
+from configs.globals import dataset_path
 from utils.datasets import ImageDataset
 
 if __name__ == "__main__":
@@ -12,7 +10,7 @@ if __name__ == "__main__":
         entity=wandb_config.ENTITY,
         name="test-load.data",
         job_type="data-load")
-    artifact = run.use_artifact(wandb_config.DATA_NAME+":latest", type="dataset-suadd")
+    artifact = run.use_artifact(wandb_config.DATA_NAME + ":latest", type="dataset-suadd")
     artifact_dir = artifact.download(root=dataset_path)
 
     dataset = ImageDataset(dataset_path, size=None)
