596049f202877438e4900e7d665eda07686334a2
axeldinh <axeldvc@gmail.com>
2023-02-10 20:46:01 +0100

    normalize by training mean and std
    if training just patchify and normalize

--- transforms/transforms_1.py
+++ transforms/transforms_1.py
@@ -2,25 +2,30 @@ import torch
 import torchvision.transforms as T
 
 from utils.transforms import RandomGamma, RandomNoise
+from utils.utils_image import patchify
 
 
 class TransformSet1:
     """
     First of transforms sets.
     Applies:
-        - Random crop in the image
         - Random Blur
         - Random Noise
+        - Color Jitter
+        - Random Gamma
+        - Random Crop
         - Random Horizontal Flip
         - Random Vertical Flip
-        - Random Brightness, Contrast, Saturation, Hue
-        - Random Gamma
         - Random Affine
         - Random Perspective
+        - Normalize
     """
 
     def __init__(self, p=0.5, patch_size=256):
 
+        self.mean = None
+        self.std = None
+        self.patch_size = patch_size
         self.value_transforms = T.Compose([
             T.GaussianBlur(kernel_size=3, sigma=(0.1, 2.0)),
             RandomNoise(0.05),
@@ -32,25 +37,42 @@ class TransformSet1:
             T.RandomCrop(size=(patch_size, patch_size)),
             T.RandomHorizontalFlip(p=p),
             T.RandomVerticalFlip(p=p),
-            T.RandomAffine(degrees=90, translate=(0.1, 0.1), scale=(0.9, 1.1), shear=10,
-                           interpolation=T.InterpolationMode.NEAREST),
-            T.RandomPerspective(distortion_scale=0.2, p=0.5, interpolation=T.InterpolationMode.NEAREST),
         ])
 
-    def __call__(self, image, semantic, depth, is_mask=False):
+    def set_mean_std(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def normalize(self, image):
+
+        if self.mean is None or self.std is None:
+            raise Exception("Mean and Std must be set before normalization.")
+
+        return T.functional.normalize(image, mean=self.mean, std=self.std)
+
+    def __call__(self, image, semantic, depth, train=True):
         """
         Applies the transforms to a batch of torch gray images.
         :param image: torch gray images (1, height, width)
-        :param is_mask: if True, only applies horizontal, vertical flips, affine and perspective transforms
+        :param semantic: torch gray images (1, height, width)
+        :param depth: torch gray images (1, height, width)
+        :param train: if True, apply the transforms, else normalize and make patches
         :return: transformed torch gray images (1, height, width)
         """
 
-        if not is_mask and image.max() > 1.0:
+        if image.max() > 1.0:
             image = image / 255.
 
-        image = self.value_transforms(image)
+        if train:
+            image = self.value_transforms(image)
+            image, semantic, depth = self.shape_transforms(torch.stack([image, semantic, depth]))
 
-        image, semantic, depth = self.shape_transforms(torch.stack([image, semantic, depth]))
+        else:
+            image = patchify(image, self.patch_size)
+            semantic = patchify(semantic, self.patch_size)
+            depth = patchify(depth, self.patch_size)
 
+        # Normalize
+        image = self.normalize(image)
 
         return image, semantic, depth
--- utils/datasets.py
+++ utils/datasets.py
@@ -17,6 +17,9 @@ class ImageDataset(Dataset):
     """
 
     def __init__(self, folder, transform=None, size=None):
+        self.train_idx = None
+        self.val_idx = None
+        self.test_idx = None
         self.images_folder = os.path.join(folder, 'inputs')
         self.semantic_annotations_folder = os.path.join(folder, 'semantic_annotations')
         self.depth_annotations_folder = os.path.join(folder, 'depth_annotations')
@@ -27,10 +30,25 @@ class ImageDataset(Dataset):
             self.images_paths = self.images_paths[:size]
         self.transform = transform
 
+        self.mean = None
+        self.std = None
+        self.compute_mean_std()
+        self.transform.set_mean_std(self.mean, self.std)
+
     def __len__(self):
         return len(self.images_paths)
 
     def __getitem__(self, idx):
+
+        if self.train_idx is None:
+            train = False
+            validation = False
+            test = False
+        else:
+            train = idx in self.train_idx
+            validation = idx in self.val_idx
+            test = idx in self.test_idx
+
         img_name = self.images_paths[idx]
         image_path = os.path.join(self.images_folder, img_name)
         semantic_annotation_path = os.path.join(self.semantic_annotations_folder, img_name)
@@ -42,16 +60,121 @@ class ImageDataset(Dataset):
         depth_annotation = imread(depth_annotation_path).astype(np.float32)
         depth_annotation = torch.from_numpy(depth_annotation).unsqueeze(0)
 
-        if self.transform:
-            image, semantic_annotation, depth_annotation = self.transform(image, semantic_annotation, depth_annotation)
+        image_shape = image.shape
+
+        if train or validation or test:
+            image, semantic_annotation, depth_annotation = self.transform(image, semantic_annotation, depth_annotation,
+                                                                          train=train)
 
         output = {
             "image": image,
             "semantic": semantic_annotation,
-            "depth": depth_annotation
+            "depth": depth_annotation,
+            "image_shape": image_shape
         }
 
         return output
+
+    def compute_mean_std(self):
+        """
+        Compute the mean and std of the training dataset
+        :return:
+        """
+        # Get mean and std for normalization
+        for i in range(len(self.images_paths)):
+            img_name = self.images_paths[i]
+            image_path = os.path.join(self.images_folder, img_name)
+            image = read_image(image_path).float()
+            if i == 0:
+                sum = 0
+                num_elements = 0
+            sum += image.sum()
+            num_elements += torch.numel(image)
+
+        self.mean = sum / num_elements
+
+        for i in range(len(self.images_paths)):
+            img_name = self.images_paths[i]
+            image_path = os.path.join(self.images_folder, img_name)
+            image = read_image(image_path).float()
+            if i == 0:
+                std = 0.0
+            std += ((image - self.mean) ** 2).sum()
+
+        self.std = torch.sqrt(std / (num_elements - 1))
+
+    def split_dataset(self, train_ratio: float, val_ratio: float) -> tuple([Subset, Subset, Subset]):
+        """
+        Splits the dataset into train validation and test.
+        The classes are equally represented in each split.
+        :param train_ratio: Ratio of train samples (between 0 and 1).
+        :param val_ratio: Ratio of validation samples (between 0 and 1).
+        :return: The train, validation and test subsets.
+        """
+        assert train_ratio + val_ratio <= 1, "Train ratio and val ratio must be less than or equal to 1"
+
+        test_ratio = 1 - train_ratio - val_ratio
+
+        # We want each class to be equally represented in each split, given the classes
+        train_idx = []
+        val_idx = []
+        test_idx = []
+
+        # Keep track of the number of images representing each class in each split
+        train_representation = np.zeros(len(wandb_config.CLASSES))
+        val_representation = np.zeros(len(wandb_config.CLASSES))
+        test_representation = np.zeros(len(wandb_config.CLASSES))
+
+        for i in range(len(self)):
+            semantic_annotation = self[i]['semantic']
+
+            class_in_image = torch.unique(semantic_annotation)
+            # Make it an integer list, and replace the 255 class with -1
+            class_in_image = [int(x.item()) for x in class_in_image]
+            class_in_image = [x if x != 255 else -1 for x in class_in_image]
+
+            # For each class add a vote if the class is under-represented in the split
+            train_votes = 0
+            val_votes = 0
+            test_votes = 0
+
+            for class_ in class_in_image:
+                # Use ratios to take into account the final number of images in each split
+                train_value = train_representation[class_] / train_ratio,
+                val_value = val_representation[class_] / val_ratio
+                test_value = test_representation[class_] / test_ratio
+                # Add a vote to the split with the least representation of the class, if all equal add to train
+                if train_value <= val_value and train_value <= test_value:
+                    train_votes += 1
+                elif val_value < train_value and val_value <= test_value:
+                    val_votes += 1
+                elif test_value < train_value and test_value < val_value:
+                    test_votes += 1
+
+            # Add the image to the split with the most votes
+            if train_votes >= val_votes and train_votes >= test_votes:
+                train_idx.append(i)
+                for class_ in class_in_image:
+                    train_representation[class_] += 1
+            elif val_votes > train_votes and val_votes >= test_votes:
+                val_idx.append(i)
+                for class_ in class_in_image:
+                    val_representation[class_] += 1
+            elif test_votes > train_votes and test_votes > val_votes:
+                test_idx.append(i)
+                for class_ in class_in_image:
+                    test_representation[class_] += 1
+
+        # Keep track of the indices for each split
+        self.train_idx = train_idx
+        self.val_idx = val_idx
+        self.test_idx = test_idx
+
+        train_dataset = torch.utils.data.Subset(self, train_idx)
+        val_dataset = torch.utils.data.Subset(self, val_idx)
+        test_dataset = torch.utils.data.Subset(self, test_idx)
+
+        return train_dataset, val_dataset, test_dataset
 
 
 def fetch_data_from_wandb():
@@ -68,73 +191,3 @@ def fetch_data_from_wandb():
     artifact = run.use_artifact(wandb_config.DATA_NAME + ":latest", type="dataset-suadd")
     artifact_dir = artifact.download()
     return artifact_dir
-
-
-def split_dataset(dataset: Dataset, train_ratio: float, val_ratio: float) -> tuple([Subset, Subset, Subset]):
-    """
-    Splits the dataset into train validation and test.
-    The classes are equally represented in each split.
-    :param dataset: Dataset to split.
-    :param train_ratio: Ratio of train samples (between 0 and 1).
-    :param val_ratio: Ratio of validation samples (between 0 and 1).
-    :return: The train, validation and test subsets.
-    """
-    assert train_ratio + val_ratio <= 1, "Train ratio and val ratio must be less than or equal to 1"
-
-    test_ratio = 1 - train_ratio - val_ratio
-
-    # If want each class to be equally represented in each split, given the classes
-    train_idx = []
-    val_idx = []
-    test_idx = []
-
-    # Keep track of the number of images representing each class in each split
-    train_representation = np.zeros(len(wandb_config.CLASSES))
-    val_representation = np.zeros(len(wandb_config.CLASSES))
-    test_representation = np.zeros(len(wandb_config.CLASSES))
-
-    for i in range(len(dataset)):
-        semantic_annotation = dataset[i]['semantic']
-
-        class_in_image = torch.unique(semantic_annotation)
-        # Make it an integer list, and replace the 255 class with -1
-        class_in_image = [int(x.item()) for x in class_in_image]
-        class_in_image = [x if x != 255 else -1 for x in class_in_image]
-
-        # For each class add a vote if the class is under-represented in the split
-        train_votes = 0
-        val_votes = 0
-        test_votes = 0
-
-        for class_ in class_in_image:
-            # Use ratios to take into account the final number of images in each split
-            train_value = train_representation[class_] / train_ratio,
-            val_value = val_representation[class_] / val_ratio
-            test_value = test_representation[class_] / test_ratio
-            # Add a vote to the split with the least representation of the class, if all equal add to train
-            if train_value <= val_value and train_value <= test_value:
-                train_votes += 1
-            elif val_value < train_value and val_value <= test_value:
-                val_votes += 1
-            elif test_value < train_value and test_value < val_value:
-                test_votes += 1
-
-        # Add the image to the split with the most votes
-        if train_votes >= val_votes and train_votes >= test_votes:
-            train_idx.append(i)
-            for class_ in class_in_image:
-                train_representation[class_] += 1
-        elif val_votes > train_votes and val_votes >= test_votes:
-            val_idx.append(i)
-            for class_ in class_in_image:
-                val_representation[class_] += 1
-        elif test_votes > train_votes and test_votes > val_votes:
-            test_idx.append(i)
-            for class_ in class_in_image:
-                test_representation[class_] += 1
-
-    train_dataset = torch.utils.data.Subset(dataset, train_idx)
-    val_dataset = torch.utils.data.Subset(dataset, val_idx)
-    test_dataset = torch.utils.data.Subset(dataset, test_idx)
-
-    return train_dataset, val_dataset, test_dataset
