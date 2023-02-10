987862857572133bf1fc37894c889225833ec562
axeldinh <axeldvc@gmail.com>
2023-02-10 18:02:08 +0100

    datasets
    changed unknown from 255 to another value (dont forget to change at the predictions)
    
    added comments

--- utils/datasets.py
+++ utils/datasets.py
@@ -11,6 +11,10 @@ import configs.wandb as wandb_config
 
 
 class ImageDataset(Dataset):
+    """
+    Dataset for semantic segmentation and depth estimation for the SUADD Challenge.
+    The transforms are applied at each iteration, so they should be random.
+    """
 
     def __init__(self, folder, transform=None, size=None):
         self.images_folder = os.path.join(folder, 'inputs')
@@ -34,30 +38,41 @@ class ImageDataset(Dataset):
 
         image = read_image(image_path).float()
         semantic_annotation = read_image(semantic_annotation_path).float()
+        semantic_annotation[semantic_annotation == 255] = len(wandb_config.CLASSES) - 1
         depth_annotation = imread(depth_annotation_path).astype(np.float32)
-        depth_annotation = torch.from_numpy(depth_annotation)
+        depth_annotation = torch.from_numpy(depth_annotation).unsqueeze(0)
 
         if self.transform:
-            image = self.transform(image)
-            semantic_annotation = self.transform(semantic_annotation, is_mask=True)
-            depth_annotation = self.transform(depth_annotation, is_mask=True)
+            image, semantic_annotation, depth_annotation = self.transform(image, semantic_annotation, depth_annotation)
 
         return image, semantic_annotation, depth_annotation
 
 
 def fetch_data_from_wandb():
+    """
+    Get the dataset from the last wandb artifact.
+    :return: The path to the dataset.
+    """
     run = wandb.init(
         project=wandb_config.WANDB_PROJECT,
         entity=wandb_config.ENTITY,
         name="download_data",
         job_type="download-data"
     )
-    artifact = run.use_artifact(wandb_config.DATA_NAME, type="dataset-suadd")
+    artifact = run.use_artifact(wandb_config.DATA_NAME + ":latest", type="dataset-suadd")
     artifact_dir = artifact.download()
     return artifact_dir
 
 
 def split_dataset(dataset: Dataset, train_ratio: float, val_ratio: float) -> tuple([Subset, Subset, Subset]):
+    """
+    Splits the dataset into train validation and test.
+    The classes are equally represented in each split.
+    :param dataset: Dataset to split.
+    :param train_ratio: Ratio of train samples (between 0 and 1).
+    :param val_ratio: Ratio of validation samples (between 0 and 1).
+    :return: The train, validation and test subsets.
+    """
     assert train_ratio + val_ratio <= 1, "Train ratio and val ratio must be less than or equal to 1"
 
     test_ratio = 1 - train_ratio - val_ratio
@@ -76,17 +91,17 @@ def split_dataset(dataset: Dataset, train_ratio: float, val_ratio: float) -> tup
         _, semantic_annotation, _ = dataset[i]
 
         class_in_image = torch.unique(semantic_annotation)
-        # Make it an interger list, and replace the 255 class with -1
+        # Make it an integer list, and replace the 255 class with -1
         class_in_image = [int(x.item()) for x in class_in_image]
         class_in_image = [x if x != 255 else -1 for x in class_in_image]
 
-        # For each class add a vote if the class is underpresented in the split
+        # For each class add a vote if the class is under-represented in the split
         train_votes = 0
         val_votes = 0
         test_votes = 0
 
         for class_ in class_in_image:
-            # Ratios to take into account the final number of images in each split
+            # Use ratios to take into account the final number of images in each split
             train_value = train_representation[class_] / train_ratio,
             val_value = val_representation[class_] / val_ratio
             test_value = test_representation[class_] / test_ratio
