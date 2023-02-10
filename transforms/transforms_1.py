387f46bf470d69d5a26d41dfed9675b74653c456
axeldinh <axeldvc@gmail.com>
2023-02-10 18:03:21 +0100

    use same transforms for all data

--- transforms/transforms_1.py
+++ transforms/transforms_1.py
@@ -1,3 +1,4 @@
+import torch
 import torchvision.transforms as T
 
 from utils.transforms import RandomGamma, RandomNoise
@@ -7,6 +8,7 @@ class TransformSet1:
     """
     First of transforms sets.
     Applies:
+        - Random crop in the image
         - Random Blur
         - Random Noise
         - Random Horizontal Flip
@@ -15,29 +17,27 @@ class TransformSet1:
         - Random Gamma
         - Random Affine
         - Random Perspective
-        - To RGB (3 channels)
     """
 
-    def __init__(self, p=0.5):
-        self.transforms = T.Compose([
+    def __init__(self, p=0.5, patch_size=256):
+
+        self.value_transforms = T.Compose([
             T.GaussianBlur(kernel_size=3, sigma=(0.1, 2.0)),
             RandomNoise(0.05),
-            T.RandomHorizontalFlip(p=p),
-            T.RandomVerticalFlip(p=p),
             T.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
             RandomGamma(0.3),
-            T.RandomAffine(degrees=90, translate=(0.1, 0.1), scale=(0.9, 1.1), shear=10),
-            T.RandomPerspective(distortion_scale=0.2, p=0.5),
         ])
 
-        self.transforms_mask = T.Compose([
+        self.shape_transforms = T.Compose([
+            T.RandomCrop(size=(patch_size, patch_size)),
             T.RandomHorizontalFlip(p=p),
             T.RandomVerticalFlip(p=p),
-            T.RandomAffine(degrees=90, translate=(0.1, 0.1), scale=(0.9, 1.1), shear=10),
-            T.RandomPerspective(distortion_scale=0.2, p=0.5),
+            T.RandomAffine(degrees=90, translate=(0.1, 0.1), scale=(0.9, 1.1), shear=10,
+                           interpolation=T.InterpolationMode.NEAREST),
+            T.RandomPerspective(distortion_scale=0.2, p=0.5, interpolation=T.InterpolationMode.NEAREST),
         ])
 
-    def __call__(self, image, is_mask=False):
+    def __call__(self, image, semantic, depth, is_mask=False):
         """
         Applies the transforms to a batch of torch gray images.
         :param image: torch gray images (1, height, width)
@@ -48,9 +48,9 @@ class TransformSet1:
         if not is_mask and image.max() > 1.0:
             image = image / 255.
 
-        if is_mask:
-            new_image = self.transforms_mask(image)
-        else:
-            new_image = self.transforms(image)
+        image = self.value_transforms(image)
 
-        return new_image
+        image, semantic, depth = self.shape_transforms(torch.stack([image, semantic, depth]))
+
+
+        return image, semantic, depth
