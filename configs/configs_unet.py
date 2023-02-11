804fec505204d7245f42734ff051cff7ce9bad42
axeldinh <axeldvc@gmail.com>
2023-02-11 17:44:45 +0100

    better config structure

--- configs/configs_unet.py
+++ configs/configs_unet.py
@@ -1,25 +1,67 @@
-import torch
-import torch.nn as nn
+import os
 
-from configs.globals import CLASSES
+import torch
+
+from configs.globals import CLASSES, OUTPUTS_PATH
+from losses.cross_entropy_mse import CrossEntropyMSE
 from models.unet import UNet
 from transforms.transforms_1 import TransformSet1
-from losses.cross_entropy_mse import CrossEntropyMSE
 
 config_1 = {
-    "model": UNet(1, 1, 2, num_classes=len(CLASSES), return_depth=True),
+    ##############################
+    # Experiment configuration
+    ##############################
+    "idx": 1,
+    "name": "Experiment_1",
+    "save_path": os.path.join(OUTPUTS_PATH, "Experiment_1"),
+
+    ##############################
+    # Data configuration
+    ##############################
+    "batch_size": 1,
+    "num_workers": 0,
+    "train_ratio": 0.95,
+    "val_ratio": 0.05,
+
+    ##############################
+    # Model configuration
+    ##############################
+    "model_args": {
+        "channels_in": 1,
+        "first_layer_channels": 1,
+        "depth": 2,
+        "num_classes": len(CLASSES),
+        "return_depth": True
+    },
+    "model": UNet,
+
+    ##############################
+    # Transform configuration
+    ##############################
+
+    "transform_args": {
+        "patch_size": 128,
+    },
+    "transform": TransformSet1,
+
+    ##############################
+    # Training configuration
+    ##############################
+
     "loss": CrossEntropyMSE(),
     "optimizer": torch.optim.Adam,
     "scheduler": None,
-    "lr": 1e-4,
-    "batch_size": 4,
-    "num_workers": 4,
-    "train_ratio": 0.8,
-    "val_ratio": 0.1,
-    "transform": TransformSet1(),
-    "epochs": 2,
+    "lr": 3e-4,
+    "epochs": 10,
     "device": "cuda" if torch.cuda.is_available() else "cpu",
-    "seed": 0,
+
+    ##############################
+    # Wandb configuration
+    ##############################
+    "monitor": "validation/loss",
+    "monitor_mode": "min",
+    "log_every_n_steps": 1,
+    "val_check_interval": 1.0
 }
 
 configs = {
