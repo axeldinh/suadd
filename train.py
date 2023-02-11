22ff34857e4ce9d2d878fc081bcafbeb25ed5d47
axeldinh <axeldvc@gmail.com>
2023-02-11 21:56:54 +0100

    option for tuning
    should be used once per model

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ import warnings
 warnings.filterwarnings("ignore", category=UserWarning, message="Checkpoint directory")
 warnings.filterwarnings("ignore", category=UserWarning, message="The dataloader,")
 
-def main(exp_id, trial_id, epochs, no_wandb, profile, command):
+def main(exp_id, trial_id, epochs, no_wandb, profile, tune_batch_size, tune_lr, command):
 
     config = load_config(exp_id, command, trial_id)
 
@@ -61,13 +61,14 @@ def main(exp_id, trial_id, epochs, no_wandb, profile, command):
         callbacks=callbacks,
         log_every_n_steps=config["log_every_n_steps"],
         val_check_interval=config["val_check_interval"],
-        auto_lr_find=True,
-        auto_scale_batch_size='power',
+        auto_lr_find=tune_lr,
+        auto_scale_batch_size='power' if tune_batch_size else None,
         num_sanity_val_steps=0,
         profiler="advanced" if profile else None,
     )
 
-    trainer.tune(model)
+    if tune_batch_size or tune_lr:
+        trainer.tune(model)
 
     model.save_hyperparameters()
 
@@ -83,6 +84,8 @@ if __name__ == "__main__":
     parser.add_argument("--no_wandb", "-w", action="store_true", help="Use wandb")
     parser.add_argument("--epochs", "-ep", type=int, help="Number of epochs", default=-1)
     parser.add_argument("--profile", "-p", action="store_true", help="Profile the code")
+    parser.add_argument("-tune_batch_size", "-tb", action="store_true", help="Tune batch size")
+    parser.add_argument("-tune_lr", "-tl", action="store_true", help="Tune learning rate")
 
     args = parser.parse_args()
 
