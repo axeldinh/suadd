8d1ba8e5f197509a80da06cf6188db16f6387576
axeldinh <axeldvc@gmail.com>
2023-02-11 02:08:50 +0100

    use metrics with torch

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,37 +1,31 @@
-import numpy as np
+import torch
 
-def abs_rel(target, prediction):
-    mask = np.isfinite(target) & (target > 0)
-    return np.mean(np.abs(target[mask] - prediction[mask]) / target[mask])
 
-def mae(target, prediction):
-    mask = np.isfinite(target) & (target > 0)
-    return np.mean(np.abs(prediction[mask] - target[mask]))
+def si_log(prediction, target):
+    mask = ~torch.isnan(target)
+    d = torch.log(prediction[mask]) - torch.log(target[mask])
+    n = torch.numel(mask)
+    si_log = torch.sum(torch.square(d), dim=0) / n - torch.sum(d, dim=0) ** 2 / n ** 2
+    return torch.sqrt(si_log) * 100.0
 
-def sq_rel(target, prediction):
-    mask = np.isfinite(target) & (target > 0)
-    sq_rel = np.mean(((target[mask] - prediction[mask]) ** 2) / target[mask])
+
+def mae(prediction, target):
+    mask = ~torch.isnan(target)
+    return torch.mean((prediction[mask] - target[mask]).abs())
+
+
+def rsme(prediction, target):
+    mask = ~torch.isnan(target)
+    rmse = (target[mask] - prediction[mask]) ** 2
+    return torch.sqrt(rmse.mean())
+
+
+def sq_rel(prediction, target):
+    mask = ~torch.isnan(target)
+    sq_rel = torch.mean(((target[mask] - prediction[mask]) ** 2) / target[mask])
     return sq_rel
 
-def si_log(target, prediction):
-    # https://proceedings.neurips.cc/paper/2014/file/7bccfde7714a1ebadf06c5f4cea752c1-Paper.pdf Section 3.2
-    mask = np.isfinite(target) & (target > 0)
-    num_vals = mask.sum()
-    log_diff = np.log(prediction[mask]) - np.log(target[mask])
-    si_log_unscaled = np.sum(log_diff**2) / num_vals - (np.sum(log_diff)**2) / (num_vals**2)
-    si_log_score = np.sqrt(si_log_unscaled)*100
-    return si_log_score
 
-def calculate_metrics(depth_annotation, depth_prediction):
-        mae_score = mae(depth_annotation, depth_prediction)
-        abs_rel_scre = abs_rel(depth_annotation, depth_prediction)
-        sq_rel_score = sq_rel(depth_annotation, depth_prediction)
-        si_log_score = si_log(depth_annotation, depth_prediction)
-        metrics = {
-                    "si_log": float(si_log_score),
-                    "abs_rel": float(abs_rel_scre),
-                    "sq_rel": float(sq_rel_score),
-                    "mae": float(mae_score)
-                  }
-        return  metrics
-
+def abs_rel(prediction, target):
+    mask = ~torch.isnan(target)
+    return torch.mean(torch.abs(target[mask] - prediction[mask]) / target[mask])
