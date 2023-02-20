705c2ded6d344d8501506148fdf23bc18e48827d
axeldinh <axeldvc@gmail.com>
2023-02-20 22:53:33 +0100

    pretrained fcn

---
+++ configs/configs_fcn.py
@@ -0,0 +1,75 @@
+import os
+
+import torch
+
+from configs.globals import OUTPUTS_PATH, CLASSES
+from losses import CrossEntropyMSE
+from models.fcn import FCN
+from transforms.transforms_1 import TransformSet1
+
+fcn_50_no_depth = {
+    ##############################
+    # Experiment configuration
+    ##############################
+    "idx": 100,
+    "name": "Experiment_1",
+    "save_path": os.path.join(OUTPUTS_PATH, "Experiment_1"),
+
+    ##############################
+    # Data configuration
+    ##############################
+    "store_images": True,  # If true, all images in the folder will be stored in memory at once
+    "number_images": None,  # Number of images contained in the whole dataset if None all images will be used
+    "batch_size": 8,
+    "num_workers": 0,
+    "train_ratio": 0.8,
+    "val_ratio": 0.1,
+
+    ##############################
+    # Model configuration
+    ##############################
+    "model_args": {
+        "resnet": 50,
+        "num_classes": len(CLASSES),
+        "return_depth": False
+    },
+    "model": FCN,
+
+    ##############################
+    # Transform configuration
+    ##############################
+
+    "transform_args": {
+        "patch_size": 512,
+    },
+    "transform": TransformSet1,
+
+    ##############################
+    # Training configuration
+    ##############################
+
+    "loss_args": {
+        "alpha": 0.5,
+        "gamma": 2,
+    },
+    "loss": CrossEntropyMSE,
+    "weighted_loss": True,
+    "optimizer": torch.optim.Adam,
+    "scheduler": None,
+    "lr": 3e-4,
+    "epochs": 100,
+    "device": "cuda" if torch.cuda.is_available() else "cpu",
+
+    ##############################
+    # Wandb configuration
+    ##############################
+    "use_wandb": True,
+    "monitor": "validation/loss",
+    "monitor_mode": "min",
+    "log_every_n_steps": 1,
+    "val_check_interval": 1.0
+}
+
+configs = {
+    100: fcn_50_no_depth,
+}
---
+++ models/fcn.py
@@ -0,0 +1,51 @@
+# Fully Convolutional Network (FCN) for semantic segmentation
+from torch import nn
+from torchvision.models.segmentation import fcn_resnet50, fcn_resnet101, FCN_ResNet50_Weights, FCN_ResNet101_Weights
+
+
+class FCN(nn.Module):
+
+    def __init__(self, num_classes, return_depth=False, resnet='50'):
+
+        super().__init__()
+
+        self.num_classes = num_classes
+        self.return_depth = return_depth
+
+        if resnet == '50':
+            self.fcn = fcn_resnet50(weights=FCN_ResNet50_Weights.DEFAULT, progress=True)
+        elif resnet == '101':
+            self.fcn = fcn_resnet101(weights=FCN_ResNet101_Weights.DEFAULT, progress=True)
+
+        # Only keep enough parameters for 1D inputs
+        param = self.fcn.backbone.conv1.weight
+        param = param[:, 0, :, :]
+        param = param.unsqueeze(1)
+        self.fcn.backbone.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.fcn.backbone.conv1.weight = nn.Parameter(param)
+
+        output_channels = num_classes
+        if return_depth:
+            output_channels += 1
+        # Change the number of classes
+        self.fcn.classifier[4] = nn.Conv2d(512, output_channels, kernel_size=1)
+        self.fcn.aux_classifier[4] = nn.Conv2d(256, output_channels, kernel_size=1)
+
+    def forward(self, x):
+        output = self.fcn(x)['out']
+        semantic = output[:, :self.num_classes, :, :]
+        depth = None
+        if self.return_depth:
+            depth = output[:, self.num_classes:, :, :]
+        return semantic, depth
+
+if __name__ == "__main__":
+    import torch
+    model = FCN(num_classes=17, return_depth=True, resnet='50')
+    x = torch.rand(1, 1, 256, 256)
+    semantic, depth = model(x)
+    print(semantic.shape, depth.shape)
+    model = FCN(num_classes=17, return_depth=True, resnet='101')
+    x = torch.rand(1, 1, 256, 256)
+    semantic, depth = model(x)
+    print(semantic.shape, depth.shape)
